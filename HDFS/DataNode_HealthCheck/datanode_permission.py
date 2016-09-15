75345253b42633c50cdf85e1512be99d7caebff6
sava7893 <vsathwika@hortonworks.com>
2016-09-15 00:46:53 -0700

    Add files via upload

--- HDFS/DataNode_HealthCheck/datanode_permission.py
+++ HDFS/DataNode_HealthCheck/datanode_permission.py
@@ -1,39 +1,61 @@
-import csv
 import re
-import datetime
-import subprocess
-from datetime import *
+from datetime import datetime
 from datetime import timedelta
 
-def reader(filepath):
-    print(filepath+" opened for processing\n")
-    return open(filepath,"r")
 
-def keyword_extractor(text_found,filevariable):
+def logFileEndTimeDetector(filepath):
+    with open(filepath, 'r') as file_to_check:
+        for line in reversed(list(file_to_check)):
+            match = re.search(r'(^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})', line)
+            if match:
+                end_time = datetime.strptime(match.group(), '%Y-%m-%d %H:%M:%S')
+                start_time = end_time -timedelta(days = 1)
+                break
+
+    return start_time
+
+
+
+def keyword_extractor(filepath,writepath,start_time,string):
+  error_list=[]
+
+  try:
+    filevariable=open(filepath, "r")
+  except Exception as error:
+    print(error)
+    return -1
   filevariable.seek(0);
-  Startup_Messages=[]
-  Errors=[]
-  #####
-  Dir=[]
   count=0
-  for line in reversed(list(filevariable)):
-    str_list = ("".join(line))
-    if text_found in str_list:
-      Errors.append(str_list)
-      Dir.append((str_list.split("/",1)[1]).split(" ",1)[0])
-      count=count+1
+  file_lines = filevariable.readlines()
+  for line in reversed(file_lines):
+      time = re.search('\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}', line)
+      if time:
+        currentTime = datetime.strptime(time.group(),'%Y-%m-%d %H:%M:%S')
+        if currentTime <=start_time:
+          break
+      if "STARTUP_MSG" in line:
+        break
+      error = re.findall(string, line)
+      if error:
+        # dir = re.search("/(.*)", line)
+        count += 1
+        error_list.append(line)
+        # if dir:
+        #     f.write("TIME: "+ str(currentTime) + " Directory Name " + dir.group(0).replace(":", "") + " ERROR TYPE " +file_lines[(file_lines.index(line))+1] )
+  with open(writepath, "a") as f:
+    if (error_list):
+      for element in error_list:
+        f.write(element)
+  return(count)
 
-  print( "Total ", count, " Instances Found\n")
-  return Startup_Messages,Errors,count,Dir
+def maincheck(read_path, write_path,string):
+  try:
+    start_time=logFileEndTimeDetector(read_path)
+    s= (keyword_extractor(read_path, write_path,start_time,string))
+    return (s)
+  except Exception as error:
+    print("File too large exception.")
+    return -1
 
-def parser(file):
-    startup_msg,errors,count,dir=keyword_extractor("Invalid dfs.datanode.data.dir",file)
-
-
-def main():
-    path="../Log_Files_For_Parsing/hadoop-hdfs-datanode-hbs1.hwxblr.com.log"
-    opened_file=reader(path)
-    parser(opened_file)
-main()
-
-##
+if __name__ == '__main__':
+  print("Call it from the Master File")
